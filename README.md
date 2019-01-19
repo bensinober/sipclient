73fb108b7d7309f907d7e5781e36d8cd85d27674
Benjamin Rokseth <bensinober@gmail.com>
2019-01-19 02:27:00 +0100

    Add client and readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# sipclient
-simple sip2 self checkout client
+# Sipclient
+
+A Simple SIP2 client for testing Self Checkout equipment.
+
+Implements a simple Socket reader repl for interacting with a SIP server
+
+# Usage
+
+```
+Usage of sipclient:
+  -adr string
+    	sip server address
+  -inst string
+    	sip institution id
+  -pass string
+    	sip login pass
+  -user string
+    	sip login user
+```   	
+
+State commands:
+
+```
+branch <branchid>
+patron <patronid>
+barcode <item id>
+```
+
+Action commands:
+
+```
+checkin
+checkout
+renew
+```
---
+++ sipclient.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/knakk/sip"
+)
+
+// Ref: https://github.com/knakk/sip/blob/master/msgdef.go
+
+func (c *Client) login() {
+	fmt.Println("LOGIN")
+	req := sip.NewMessage(sip.MsgReqLogin)
+	req.AddField(c.user)
+	req.AddField(c.pass)
+	req.AddField(sip.Field{Type: sip.FieldUIDAlgorithm, Value: "0"})
+	req.AddField(sip.Field{Type: sip.FieldPWDAlgorithm, Value: "0"})
+
+	fmt.Println(req.String())
+	res := c.send(req)
+	fmt.Println(res.String())
+	return
+}
+
+func (c *Client) checkin() {
+	if c.State["barcode"] == "" || c.State["branch"] == "" {
+		fmt.Println("Missing barcode or branch!")
+		return
+	}
+	fmt.Printf("checking in barcode %s at %s\n", c.State["barcode"], c.State["branch"])
+	req := sip.NewMessage(sip.MsgReqCheckin)
+	//req.AddField(c.inst)
+	t := sipTime()
+	req.AddField(sip.Field{Type: sip.FieldNoBlock, Value: "Y"})
+	req.AddField(sip.Field{Type: sip.FieldTransactionDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldReturnDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldInstitutionID, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldTerminalLocation, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldItemIdentifier, Value: c.State["barcode"]})
+
+	res := c.send(req)
+	fmt.Println(res.String())
+	return
+}
+
+func (c *Client) checkout() {
+	if c.State["barcode"] == "" || c.State["branch"] == "" || c.State["patron"] == "" {
+		fmt.Println("Missing patron, barcode or branch!")
+		return
+	}
+	fmt.Printf("checking out barcode %s for %s at %s\n", c.State["barcode"], c.State["patron"], c.State["branch"])
+	req := sip.NewMessage(sip.MsgReqCheckout)
+	//req.AddField(c.inst)
+	t := sipTime()
+	req.AddField(sip.Field{Type: sip.FieldRenewalPolicy, Value: "Y"})
+	req.AddField(sip.Field{Type: sip.FieldNoBlock, Value: "N"})
+	req.AddField(sip.Field{Type: sip.FieldTransactionDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldNbDueDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldInstitutionID, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldTerminalLocation, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldItemIdentifier, Value: c.State["barcode"]})
+	req.AddField(sip.Field{Type: sip.FieldPatronIdentifier, Value: c.State["patron"]})
+
+	fmt.Println(req.String())
+	res := c.send(req)
+	fmt.Println(res.String())
+	return
+}
+
+func (c *Client) renew() {
+	if c.State["barcode"] == "" || c.State["branch"] == "" || c.State["patron"] == "" {
+		fmt.Println("Missing patron, barcode or branch!")
+		return
+	}
+	fmt.Printf("renewing barcode %s for %s at %s\n", c.State["barcode"], c.State["patron"], c.State["branch"])
+	req := sip.NewMessage(sip.MsgReqRenew)
+	//req.AddField(c.inst)
+	t := sipTime()
+	req.AddField(sip.Field{Type: sip.FieldThirdPartyAllowd, Value: "Y"})
+	req.AddField(sip.Field{Type: sip.FieldNoBlock, Value: "Y"})
+	req.AddField(sip.Field{Type: sip.FieldTransactionDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldNbDueDate, Value: t})
+	req.AddField(sip.Field{Type: sip.FieldPatronIdentifier, Value: c.State["patron"]})
+	req.AddField(sip.Field{Type: sip.FieldInstitutionID, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldTerminalLocation, Value: c.State["branch"]})
+	req.AddField(sip.Field{Type: sip.FieldItemIdentifier, Value: c.State["barcode"]})
+
+	res := c.send(req)
+	fmt.Println(res.String())
+	return
+}
+
+func (c *Client) send(req sip.Message) sip.Message {
+	if _, err := req.Encode(c.Conn); err != nil {
+		log.Println("error writing SIP request to remote: " + err.Error())
+		return sip.Message{}
+	}
+	rBuf := make([]byte, 16*1024) // read buffer
+	n, err := c.Conn.Read(rBuf)
+	if err != nil {
+		if err != io.EOF {
+			log.Println("error reading SIP server response: " + err.Error())
+		}
+		return sip.Message{}
+	}
+	res, err := sip.Decode(rBuf[:n])
+	if err != nil {
+		log.Println("error decoding SIP response: " + err.Error())
+		return sip.Message{}
+	}
+	return res
+}
+
+func sipTime() string {
+	t := time.Now()
+	return fmt.Sprintf("%s", t.Format("20060102    150405"))
+}
+
+func readStdin(c *Client) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		line, _ := reader.ReadString('\n')
+		cmd := strings.Fields(line)
+		switch cmd[0] {
+		/* --- state commands --- */
+		case "state":
+			fmt.Printf("status:\n----\n%v\n", c.State)
+		case "branch":
+			c.State["branch"] = cmd[1]
+		case "barcode":
+			c.State["barcode"] = cmd[1]
+		case "patron":
+			c.State["patron"] = cmd[1]
+		/* --- action commands --- */
+		case "checkin":
+			c.checkin()
+		case "checkout":
+			c.checkout()
+		case "renew":
+			c.renew()
+		default:
+			fmt.Println("UNKNOWN COMMAND")
+		}
+	}
+	//c.Conn.Write([]byte(msg))
+}
+
+type Client struct {
+	user  sip.Field
+	pass  sip.Field
+	inst  sip.Field
+	Conn  net.Conn
+	State map[string]string // simplest possible state map for patron, branch, barcode etc
+}
+
+func newClient(adr string, u string, p string, i string) *Client {
+	// Clientect to remote.
+	c, err := net.Dial("tcp", adr)
+	if err != nil {
+		log.Println("error connecting to remote: " + err.Error())
+		return nil
+	}
+	return &Client{
+		user:  sip.Field{Type: sip.FieldLoginUserID, Value: u},
+		pass:  sip.Field{Type: sip.FieldLoginPassword, Value: p},
+		inst:  sip.Field{Type: sip.FieldInstitutionID, Value: i},
+		Conn:  c,
+		State: make(map[string]string),
+	}
+}
+
+func main() {
+	http.DefaultClient.Timeout = 5 * time.Second
+	var (
+		adr  = flag.String("adr", "", "sip server address")
+		user = flag.String("user", "", "sip login user")
+		pass = flag.String("pass", "", "sip login pass")
+		inst = flag.String("inst", "", "sip institution id")
+	)
+	flag.Parse()
+
+	if *adr == "" || *user == "" || *pass == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	c := newClient(*adr, *user, *pass, *inst)
+	fmt.Printf("connected to to: %s\n", c.Conn.RemoteAddr().String())
+	c.login()
+	defer c.Conn.Close()
+	readStdin(c)
+}
